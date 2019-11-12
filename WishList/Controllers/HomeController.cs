34095b76b5bdb7a370c3a5959197ae91a14d38b8
Iryna Pashenkovska <i.pashenkovska@gmail.com>
2019-11-12 13:07:03 +0200

    Add Home Controller and views

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Home")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+    }
+}
